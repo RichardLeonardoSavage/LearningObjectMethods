1cf6451d82ff18163027a4daf0adaf72553d4aa5
RichardLeonardoSavage <savagerichard100@gmail.com>
2022-08-08 17:18:57 +1200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningObjectMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //these are objects created from the class 
+            Student student1 = new Student("Jim", "Business", 2.8);
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+
+            //here we are calling upon a method created over on another file
+            Console.WriteLine("Does " + student1.name + " have honors? " + student1.HasHonors());
+            Console.WriteLine("Does " + student2.name + " have honors? " + student2.HasHonors());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningObjectMethods
+{
+
+
+    //this is a class that has been assigned properties
+     class Student
+    {
+        public string name;
+        public string major;
+        public double gpa;
+
+
+
+        // here is a constructor that we have created for this classes objects
+        public Student(string aName, string aMajor, double GPA)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = GPA;
+        }
+
+
+        //here we made a method to determine if one of the objects properties meets a certain critera and returns an answer
+        public bool HasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+    }
+}
